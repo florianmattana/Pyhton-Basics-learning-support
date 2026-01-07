1d6b4b8846d7609a4f365f6db59656b8746e31e4
florianmattana <mattana.florian@gmail.com>
2026-01-07 15:51:44 +0100

    second exo to support python learning

---
+++ exo1-2.py
@@ -0,0 +1,18 @@
+# Exercise 1.2 - Temperature Converter
+    # Ask for a temperature in Celsius
+    # Convert it to Fahrenheit (formula: F = C × 9/5 + 32)
+    # Convert it to Kelvin (formula: K = C + 273.15)
+    # Display formatted results
+
+celsius = float(input("What is the Celsius temp : "))
+
+fahrenheit = (celsius * (9/5)) + 32
+print(celsius, "celsius is", fahrenheit, "Fahrenheit !")
+
+kelvin = celsius + 273.15
+print(celsius, "celsius is", kelvin, "Kelvin !")
+
+#or
+
+print(f"{celsius} Celsius is {fahrenheit} Fahrenheit!")
+print(f"{celsius} Celsius is {kelvin} Kelvin!")
